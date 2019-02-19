811653d7f6d431cac6cf5004838f8d95a2ac1492
Pushpank <pushpankq@gmail.com>
2019-02-19 16:16:24 +0530

    Update APIManager.swift

--- APIManager.swift
+++ APIManager.swift
@@ -14,23 +14,23 @@ class APIManager {
     static let shared = APIManager()
     
     private init() {
+        
+        // Do nothing here...
     }
     
     // Completion Handler
     typealias webServiceResponse = (JSON?, Error?) -> Void
     
-    
     // MARK: Get Service Function
-    
     func getService (getUrl: String, completionHandler : @escaping (JSON?, Error?) -> ()) {
       
+        // If Url is not valid 
         guard let url = URL(string: getUrl) else {
             print("Error: cannot create URL")
             return
         }
         
         let config = URLSessionConfiguration.default
-        
         let session = URLSession(configuration: config)
         
         let task = session.dataTask(with: url) { (data, response, error) in
@@ -54,14 +54,15 @@ class APIManager {
     
     
     // MARK: Post service function
-    
     func postService (postUrl: String, dict: [String:Any], completionHandler: @escaping webServiceResponse ) {
        
         let Url = String(format: postUrl)
         
+        // Url Validate 
         guard let serviceUrl = URL(string: Url) else { return }
         
         var request = URLRequest(url: serviceUrl)
+        
         request.httpMethod = "POST"
         request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
         guard let httpBody = try? JSONSerialization.data(withJSONObject: dict, options: []) else {
@@ -71,17 +72,23 @@ class APIManager {
         request.httpBody = httpBody
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
+                                         
             if let response = response {
                 print("response is ",response)
             }
+                                         
             if let data = data {
                 do {
                     let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    
+                    // convert objects in json objects 
                     let jsonPayload = JSON(json)
+                    // if you get the data, callback on the model call 
                     completionHandler(jsonPayload, nil)
                     print("JSON Serialization **** ",json)
                 } catch {
                     print("error is *** ",error)
+                    // If you get the error, callback with error in model class
                     completionHandler(nil, error)
                 }
             }
